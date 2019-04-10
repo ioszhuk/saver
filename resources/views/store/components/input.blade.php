22075d4cf04016b27751af05a6ee06e612458c69
Kirill Zhukovskiy <ioszhuk@gmail.com>
2019-04-10 13:40:07 +0300

    fix

---
+++ resources/views/store/components/input.blade.php
@@ -0,0 +1,6 @@
+<div class="media text-muted pt-3">
+    <div class="col-12">
+        <div id="alert-response"></div>
+        <input id="file-uploader" type="text" name="url" class="form-control" placeholder="Paste URL to start file uploading" required="required">
+    </div>
+</div>
--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -4,12 +4,9 @@
 
     <div class="my-3 p-3 bg-white rounded box-shadow">
         <h6 class="border-bottom border-gray pb-2 mb-0">File uploader</h6>
-        <div class="media text-muted pt-3">
-            <div class="col-12">
-                <div id="alert-response"></div>
-                <input id="file-uploader" type="text" name="url" class="form-control" placeholder="Paste URL to start file uploading" required="required">
-            </div>
-        </div>
+
+        @include('store.components.input')
+
     </div>
 
     <div class="my-3 p-3 bg-white rounded box-shadow">
